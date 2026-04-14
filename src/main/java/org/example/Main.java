b7038b0458330475e8813b855f4b2448e4caea33
MateuszN313 <mateusznieweglowski10@gmail.com>
2026-04-14 15:35:02 +0200

    added vehicle category config and refactored UI and services

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,42 @@
-package org.example;
+package com.umcsuser.carrent;
 
-import org.example.repositories.RentalRepository;
-import org.example.repositories.UserRepository;
-import org.example.repositories.VehicleRepository;
-import org.example.repositories.impl.RentalJsonRepository;
-import org.example.repositories.impl.UserJsonRepository;
-import org.example.repositories.impl.VehicleJsonRepository;
-import org.example.services.AuthService;
-import org.example.services.RentalService;
-import org.example.services.UserService;
-import org.example.services.VehicleService;
+import com.umcsuser.carrent.repositories.RentalRepository;
+import com.umcsuser.carrent.repositories.UserRepository;
+import com.umcsuser.carrent.repositories.VehicleCategoryConfigRepository;
+import com.umcsuser.carrent.repositories.VehicleRepository;
+import com.umcsuser.carrent.repositories.impl.RentalJsonRepository;
+import com.umcsuser.carrent.repositories.impl.UserJsonRepository;
+import com.umcsuser.carrent.repositories.impl.VehicleCategoryConfigJsonRepository;
+import com.umcsuser.carrent.repositories.impl.VehicleJsonRepository;
+import com.umcsuser.carrent.services.AuthService;
+import com.umcsuser.carrent.services.RentalService;
+import com.umcsuser.carrent.services.UserService;
+import com.umcsuser.carrent.services.VehicleCategoryConfigService;
+import com.umcsuser.carrent.services.VehicleService;
+import com.umcsuser.carrent.services.VehicleValidator;
 
 public class Main {
     public static void main(String[] args) {
         VehicleRepository vehicleRepository = new VehicleJsonRepository();
         UserRepository userRepository = new UserJsonRepository();
         RentalRepository rentalRepository = new RentalJsonRepository();
+        VehicleCategoryConfigRepository categoryConfigRepository = new VehicleCategoryConfigJsonRepository();
 
         AuthService authService = new AuthService(userRepository);
-        RentalService rentalService = new RentalService(rentalRepository);
-        UserService userService = new UserService(userRepository);
-        VehicleService vehicleService = new VehicleService(vehicleRepository);
+        VehicleCategoryConfigService categoryConfigService = new VehicleCategoryConfigService(categoryConfigRepository);
+        VehicleValidator vehicleValidator = new VehicleValidator(categoryConfigService);
+        VehicleService vehicleService = new VehicleService(vehicleRepository, rentalRepository, vehicleValidator);
+        RentalService rentalService = new RentalService(rentalRepository, vehicleRepository);
+        UserService userService = new UserService(userRepository, rentalService);
 
-        UI client = new UI(authService, rentalService, userService, vehicleService);
-        client.start();
+        UI ui = new UI(
+                authService,
+                vehicleService,
+                rentalService,
+                userService,
+                categoryConfigService
+        );
+
+        ui.start();
     }
 }
---
+++ src/main/java/org/example/repositories/impl/VehicleCategoryConfigJsonRepository.java
@@ -0,0 +1,2 @@
+package org.example.repositories.impl;public class VehicleCategoryConfigJsonRepository {
+}

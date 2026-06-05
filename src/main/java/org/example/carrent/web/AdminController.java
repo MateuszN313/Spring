fb23913d496039c5f233b15155557a5d06f7701d
mateusz <mateusznieweglowski10@gmail.com>
2026-06-05 14:34:34 +0200

    fix admin problem

--- src/main/java/org/example/carrent/web/AdminController.java
+++ src/main/java/org/example/carrent/web/AdminController.java
@@ -3,18 +3,38 @@ package org.example.carrent.web;
 import lombok.RequiredArgsConstructor;
 import org.example.carrent.models.Rental;
 import org.example.carrent.models.User;
+import org.example.carrent.models.Vehicle;
 import org.example.carrent.services.RentalServiceInterface;
 import org.example.carrent.services.UserServiceInterface;
+import org.example.carrent.services.VehicleServiceInterface;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-@RequiredArgsConstructor
 public class AdminController {
+    VehicleServiceInterface vehicleService;
     RentalServiceInterface rentalService;
     UserServiceInterface userService;
+
+    public AdminController(VehicleServiceInterface vehicleService, RentalServiceInterface rentalService, UserServiceInterface userService) {
+        this.vehicleService = vehicleService;
+        this.rentalService = rentalService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/vehicles")
+    public Vehicle vehicleCreate(@RequestBody Vehicle vehicle){
+        return vehicleService.addVehicle(vehicle);
+    }
+
+    @DeleteMapping("/vehicles/{id}")
+    public ResponseEntity<Void> vehicleDelete(@PathVariable String id){
+        this.vehicleService.removeVehicle(id);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping("/rentals")
     public List<Rental> rentalsList(){
